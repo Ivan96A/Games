b5f2983c213f7d729337b7d897084b2e79801688
Иван Арабчук < >
2016-10-20 16:49:05 +0300

    first commit

---
+++ src/main/java/computer/games/Application.java
@@ -0,0 +1,23 @@
+package computer.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Ivan on 20.10.2016.
+ */
+@SpringBootApplication
+@ComponentScan
+public class Application extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
