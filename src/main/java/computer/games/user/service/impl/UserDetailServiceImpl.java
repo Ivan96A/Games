855f327cab87364efa820467176e50abb78e1bae
Nicholas Grynkevych <nicholasg@rambler.ru>
2017-01-22 14:26:49 +0200

    Fixed auth

--- src/main/java/computer/games/user/service/impl/UserDetailServiceImpl.java
+++ src/main/java/computer/games/user/service/impl/UserDetailServiceImpl.java
@@ -2,14 +2,15 @@ package computer.games.user.service.impl;
 
 import computer.games.user.domain.CustomUser;
 import computer.games.user.repository.UserRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -21,16 +22,22 @@ import java.util.Set;
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
+    private static final Logger LOGGER = Logger.getLogger(UserDetailServiceImpl.class);
+
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        CustomUser customUser = userRepository.findByUsername(s);
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        LOGGER.info("Init parameters method loadUserByUsername: " + email);
+        CustomUser customUser = userRepository.findByUsername(email);
         User user;
+        if (customUser == null) {
+            throw new BadCredentialsException(email);
+        }
         final Set<GrantedAuthority> authorities = new HashSet<>();
         authorities.add(new SimpleGrantedAuthority(customUser.getRole()));
-        user = new User(customUser.getUsername(), customUser.getPassword(), authorities );
+        user = new User(customUser.getUsername(), customUser.getPassword(), authorities);
         return user;
     }
 
